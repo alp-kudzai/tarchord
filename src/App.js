27f3257b2994c4aa5924fffffdcf794d6cf85bb3
Kudzai <alpha.kudzai@gmail.com>
2023-03-15 15:47:36 +0200

    it works on gh-pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const API = "https://api.uberchord.com/v1/embed/chords?nameLike="
+  function submitSearch(e){
+    e.preventDefault()
+    let term = document.getElementById("searchbar").value
+    let chordSpace = document.getElementById("ChordSpace")
+    let newIframe = document.createElement("iframe")
+    newIframe["src"] = API+term
+    newIframe["title"] = "Chords"
+    newIframe["class"] = "chordFrames"
+    chordSpace.appendChild(newIframe)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1 id="TitleBar">Tarchord</h1>
+        <h3 id="subtitleBar">Google for Guitar chords</h3>
+        <input type="text" id="searchbar"/>
+        <input type="submit" id="submitButton" onClick={submitSearch}/>
       </header>
+      
+      <div id="ChordSpace">
+
+      </div>
     </div>
   );
 }
